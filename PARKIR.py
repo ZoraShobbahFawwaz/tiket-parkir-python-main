9d14867a538ac864367fc1e3492f11b0dd5cad90
ZoraShobbahFawwaz <zorafawwaz8@gmail.com>
2023-01-19 20:30:24 +0700

    penambahan fungsi delete

---
+++ PARKIR.py
@@ -0,0 +1,15 @@
+
+
+def data_kendaraan(data_file):
+    no = "NO"
+    jenis = "JENIS KENDARAAN"
+    plat = "PLAT KENDARAAN"
+    nama = "NAMA KENDARAAN"
+    jam_masuk = "JAM MASUK"
+    jam_keluar = "JAM KELUAR"
+    total_jam = "TOTAL JAM"
+    total_bayar = "TOTAL BAYAR"
+
+    print("="*56)
+    print(f"{no:1}  {no:10} {jenis:10}  {plat:10}   {nama:10}   {jam_masuk:10}  {jam_keluar:10} {total_jam:10}  {total_bayar:10}    ")
+    print("-"*56)
---
+++ test.py
@@ -0,0 +1,4 @@
+listd = [["truk",1234,142224],["mobil",1234,142224]]
+print(listd)
+listd.sort(key=len)
+print(listd)
